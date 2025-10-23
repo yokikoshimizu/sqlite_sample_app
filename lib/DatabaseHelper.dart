1592cdd18fc5339d6a4333f93318d1b578109d5a
C:\Users\heppo <yokikoshimizu@gmail.com>
2025-10-24 04:45:50 +0900

    A. データベースの初期化とオープン (Create Table)までを追加

---
+++ lib/DatabaseHelper.dart
@@ -0,0 +1,42 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+
+class DatabaseHelper {
+  static final DatabaseHelper instance = DatabaseHelper._init();
+  static Database? _database;
+
+  DatabaseHelper._init();
+
+  Future<Database> get database async {
+    if(_database != null) return _database!;
+    _database = await _initDB('my_database.db');
+    return _database!;
+  }
+
+  Future<Database> _initDB(String filePath) async {
+    final dbPath = await getDatabasesPath();
+    final path = join(dbPath, filePath);
+
+    return await openDatabase(
+      path,
+      version: 1,
+      onCreate: _createDB,
+    );
+  }
+
+  Future _createDB(Database db, int version) async {
+    const idType = 'INTEGER PRIMARY KEY AUTOINCREMENT';
+    const textType = 'TEXT NOT NULL';
+    const integerType = 'INTEGER NOT NULL';
+
+    await db.cxecute('''
+    CREATE TABLE dogs (
+      id $idType,
+      name $textType,
+      age $integerType
+    )
+    ''');
+  }
+
+  
+}
